5d32d6d0acb6af0638ffb4e06217e877fd0f2646
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2026-04-26 20:40:52 -0300

    Visualização da forma do polígono escolhido

--- aula01/script.js
+++ aula01/script.js
@@ -7,6 +7,28 @@ document.querySelector("#botao").addEventListener("click", () =>{
     const ladoB = Number(document.querySelector("#ladoB").value)
     const res = document.querySelector("#res")
 
+    document.querySelector("#ladoA").classList.remove("input-erro")
+    document.querySelector("#ladoB").classList.remove("input-erro")
+    res.textContent = ""
+    let erro = false
+
+    if (!ladoA || ladoA <= 0){
+        document.querySelector("#ladoA").classList.add("input-erro")
+        erro = true
+        console.log(erro+"1")
+    }
+    if (formaSelecionada === "retangulo" && (!ladoB || ladoB <= 0)){
+        document.querySelector("#ladoB").classList.add("input-erro")
+        erro = true
+    }
+    if (erro) {
+        res.textContent = "Preencha os dados corretamente!"
+        res.style.color = "red"
+        return 
+    }
+
+    res.style.color = "var(--corSecundaria)"
+
     let perimetro
 
     if (formaSelecionada == "quadrado"){perimetro = ladoA*4}
@@ -23,11 +45,22 @@ document.querySelector("#botao").addEventListener("click", () =>{
 // Troca a formula do perimetro e altera o formato do poligo de visualização
 document.querySelectorAll('input.forma').forEach(input => {input.addEventListener("change", () =>{
 
+        document.querySelector("#ladoA").classList.remove("input-erro")
+        document.querySelector("#ladoB").classList.remove("input-erro")
+
         const prevFormula = document.querySelector("#prevFormula")
+        const visuForma = document.querySelector("#visuForma")
+
         const formulas = {
             'quadrado': 'Perímetro = lado A x 4',
             'triangulo': 'Perímetro = lado A x 3',
             'retangulo': 'Perímetro = 2 x (lado A + lado B)'
+        }
+
+        const classesAnimacao = {
+            'quadrado': 'forma-quadrado',
+            'triangulo': 'forma-triangulo',
+            'retangulo': 'forma-retangulo'
         }
         
         prevFormula.classList.add("escondido")
@@ -35,7 +68,10 @@ document.querySelectorAll('input.forma').forEach(input => {input.addEventListene
         setTimeout(() => {
             prevFormula.textContent = formulas[input.value]
             prevFormula.classList.remove("escondido")
-        }, 300);
+        }, 200);
+
+        visuForma.classList.remove('forma-quadrado', 'forma-triangulo', 'forma-retangulo');
+        visuForma.classList.add(classesAnimacao[input.value])
         
     })    
 })
