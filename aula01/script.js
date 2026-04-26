48c948bf2d0257c48b441ab52c65bdfd35153058
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2026-04-26 13:08:30 -0300

    Mudança na formula
    A formula do perímetro muda dependendo de qual foi a forma selecionada

--- aula01/script.js
+++ aula01/script.js
@@ -7,8 +7,35 @@ document.querySelector("#botao").addEventListener("click", () =>{
     const ladoB = Number(document.querySelector("#ladoB").value)
     const res = document.querySelector("#res")
 
-    let perimetro = formaSelecionada === 'quadrado' ? 4*ladoA : formaSelecionada === 'triangulo' ? 3*ladoA : formaSelecionada === 'retangulo' ? 2*(ladoA+ladoB) : 0
+    let perimetro
 
+    if (formaSelecionada == "quadrado"){perimetro = ladoA*4}
+    if (formaSelecionada == "triangulo"){perimetro = ladoA*3}
+    if (formaSelecionada == "retangulo"){perimetro = 2*(ladoA+ladoB)}
+
+    document.querySelector("#ladoA").value = ""
+    document.querySelector("#ladoB").value = ""
+    
     res.innerHTML = `O perímetro é de: ${perimetro}cm`
     
+})
+
+// Troca a formula do perimetro e altera o formato do poligo de visualização
+document.querySelectorAll('input.forma').forEach(input => {input.addEventListener("change", () =>{
+
+        const prevFormula = document.querySelector("#prevFormula")
+        const formulas = {
+            'quadrado': 'Perímetro = lado A x 4',
+            'triangulo': 'Perímetro = lado A x 3',
+            'retangulo': 'Perímetro = 2 x (lado A + lado B)'
+        }
+        
+        prevFormula.classList.add("escondido")
+        
+        setTimeout(() => {
+            prevFormula.textContent = formulas[input.value]
+            prevFormula.classList.remove("escondido")
+        }, 300);
+        
+    })    
 })
